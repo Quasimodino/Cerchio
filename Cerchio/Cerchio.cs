3b4f4236b162dd61407673a63e443dc1ce2e74fa
atadd <atadd@DESKTOP-PVF3S8C>
2024-01-24 17:27:39 +0100

    Aggiungere i file di progetto.

---
+++ Cerchio/Cerchio.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cerchio
+{
+    internal class Cerchio
+    {
+        public double raggio { get; set; }
+        public double calcoloarea()
+        {
+            double area;
+            area = raggio * raggio * Math.PI;
+            return area;
+        }
+        public double calcoloperimetro()
+        {
+            double perimetro;
+            perimetro = 2 * Math.PI * raggio;
+            return perimetro;
+        }
+    }
+}
---
+++ Cerchio/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cerchio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cerchio cerchio = new Cerchio();
+            Console.WriteLine(" ");
+            cerchio.raggio=double.Parse(Console.ReadLine());
+            Console.WriteLine("{0}", cerchio.calcoloperimetro());
+            Console.WriteLine("{0}", cerchio.calcoloarea());
+            Console.ReadLine();
+
+        }
+    }
+}
